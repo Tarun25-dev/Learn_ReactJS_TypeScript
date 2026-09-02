d3e4439059b071ae2e9bc2748057aa84380f28a6
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-09-02 19:55:58 +0530

    Add files via upload

---
+++ Day2/App.tsx
@@ -0,0 +1,14 @@
+import App from "./expressions";
+import MulExp from "./multipleExp";
+
+function Main() {
+    return(
+        <>
+        <App />
+        <hr />
+        <MulExp />
+        </>
+    )
+}
+
+export default Main;
